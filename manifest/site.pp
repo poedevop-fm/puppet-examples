cae3f3120aabfc29c21184c87c887bb569e97adf
herve <rv.meftah@gmail.com>
2015-12-11 14:09:33 +0000

    added package haproxy

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,4 +1,13 @@
-notify {
-   'Hello, world !' :
+package {
+	'haproxy': ensure => 'installed',
+	}
+file {
+	'/etc/haproxy/haproxy.cfg':
+	owner => 'root',
+	group => 'root',
+	mode => '644',
+	source => 'puppet:///modules/haproxy/etc/haproxy/haproxy.cfg',
 }
-
+service {
+	'haproxy': ensure => 'running',
+}
