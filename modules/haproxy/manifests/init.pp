ad68f39a77d9564ef60d4e967e441e28627d20b5
Herve <rv.meftah@gmail.com>
2015-12-14 16:54:58 +0000

    added a whole filebucket example

---
+++ modules/haproxy/manifests/init.pp
@@ -0,0 +1,41 @@
+class  haproxy::install {
+        package { "haproxy":
+                name => $operatingsystem ? {
+                        /(Red Hat|CentOS|Fedora)/ => "haproxy",
+                        Ubuntu => "haproxy",
+                        default=> "haproxy"
+                 },
+                 ensure => latest,
+        }
+}
+
+class haproxy::config {
+        file {"/etc/haproxy/haproxy.cfg":
+                        ensure => present,
+                        owner  => 'root',
+                        group  => 'root',
+                        mode   => 0600,
+                        source => "puppet:///modules/haproxy/haproxy.cfg",
+                        require=> Class["haproxy::install"],
+                        notify => Class["haproxy::service"],
+        }
+}
+
+class haproxy::service {
+        service { "Ãhaproxy":
+                        name => $operatingsystem ? {
+                           /(Red Hat|CentOS|Fedora)/ => "haproxy",
+                           Ubuntu => "haproxy",
+                           default=> "haproxy"
+                        },
+                        ensure     => running,
+                        hasstatus  => true,
+                        hasrestart => true,
+                        enable     => true,
+                        require    => Class["haproxy::config"],
+        }
+}
+
+class haproxy {
+        include haproxy::install, haproxy::config, haproxy::service
+}
